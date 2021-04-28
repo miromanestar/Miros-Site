c6494bfb84d07b7a744938c706eb3802c2057065
Miro Manestar <miroimanestar@gmail.com>
2021-04-28 00:42:35 -0400

    Updated some variable definitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,12 +78,12 @@ function loadContent(selection, state, changeState) {
 }
 
 function insertBreadcrumbs(selection) {
-    triangle = `<svg style="transform: rotate(90deg); font-size: .7em;" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-triangle-fill ml-2 mr-2 my-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    const triangle = `<svg style="transform: rotate(90deg); font-size: .7em;" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-triangle-fill ml-2 mr-2 my-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/>
         </svg>`;
-
-    pathArr = selection.split('/');
-    breadcrumb = `<li class="breadcrumb-item active">Home</li>`;
+        
+    const pathArr = selection.split('/');
+    let breadcrumb = `<li class="breadcrumb-item active">Home</li>`;
     if(selection != 'home' && pathArr.length > 0) {
         breadcrumb = `<li class="breadcrumb-item"><a class="hover-scale" role="button" onclick="loadContent('home')">Home</a></li>`;
         for(let i = 0; i < pathArr.length; i++) {
